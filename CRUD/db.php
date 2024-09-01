8521223cd16edcf2d9ffe77a4c09f4e698d4368b
Suraj Kumar <surajkameshwarprasad@gmail.com>
2024-09-01 15:38:18 +0530

    Database File

---
+++ CRUD/db.php
@@ -0,0 +1,22 @@
+<?php
+$serverName = "localhost";
+$userName = "root";
+$password = "";
+$dbName = "crud2";
+
+
+//Create a connection
+$conn = mysqli_connect($serverName,$userName,$password,$dbName);
+if(!$conn){
+    die("Could Not Connect".mysqli_error());
+}else{
+    echo "Connection Successfully.";
+    //* $sql = "create database crud2";
+    //* $result = mysqli_query($conn,$sql);
+    //* if($result){
+    //*      echo "Database Created Success!";
+    //* }else{
+    //*     echo "Database Not Created!";
+    //* }
+}
+?>
